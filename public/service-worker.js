18cb3fed0cdbc4f1860445be87e227aed106e7d8
Jesse Orozco <orozcojesse0@gmail.com>
2021-06-25 18:51:14 -0400

    added index.db file included db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -51,7 +51,7 @@ self.addEventListener("fetch", (event) => {
 
         return caches.open(RUNTIME_CACHE).then((cache) => {
           return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
+            return cache.put(event.request.url, response.clone()).then(() => {
               return response;
             });
           });
